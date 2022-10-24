d389b0228d98588fd9b2eb635605f45758cdbefe
Andreas Korth <andreas.korth@gmail.com>
2022-10-24 10:35:44 +0200

    Cleaned up App component

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,25 @@
 <script setup lang="ts">
 import { ref, onMounted } from "vue";
 import { dia, shapes } from "jointjs";
+import { debounce } from "lodash";
 
 import NodeDrawer from "@/components/NodeDrawer.vue";
 import WorkfLowView from "@/components/WorkflowView.vue";
-import { debounce } from "lodash";
 
 const graph = ref<dia.Graph>(new dia.Graph({}, { cellNamespace: shapes }));
 
-const save = debounce(() => {
+function save() {
   localStorage.setItem("workflow-graph", JSON.stringify(graph.value.toJSON()));
-}, 250);
+}
 
-const load = () => {
+function load() {
   const json = localStorage.getItem("workflow-graph");
   if (json) graph.value.fromJSON(JSON.parse(json));
-};
+}
 
 onMounted(() => {
   load();
-  graph.value.on("add change remove", save);
+  graph.value.on("add change remove", debounce(save, 250));
 });
 </script>
 
